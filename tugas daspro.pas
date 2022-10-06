507879aef0cba704b6df8a11999e33808dae0858
Notuwapu <prawirasaragih1@gmail.com>
2022-10-06 19:59:27 +0700

    Tugas Pertama Saya

---
+++ tugas daspro.pas
@@ -0,0 +1,15 @@
+var
+jalan,kota : string;
+nomor,kodepos : integer;
+begin
+Write('Jalan    : ');
+readln(jalan);
+Write('No       : ');
+readln(nomor);
+Write('Kota     : ');
+readln(kota);
+Write('Kode Pos : ');
+readln(kodepos);
+writeln('');
+writeln('Alamat : Jalan ',jalan,' ','No ',nomor,' ',kota,' ',kodepos);
+end.
